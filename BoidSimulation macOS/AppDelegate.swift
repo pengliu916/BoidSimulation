004205efd2efae4c610957d9bf165ea7bb9c921d
Peng <pliu3@apple.com>
2018-05-13 15:03:36 -0700

    Initial Commit

---
+++ BoidSimulation macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  BoidSimulation macOS
+//
+//  Created by Peng Liu on 5/13/18.
+//  Copyright © 2018 PLStudio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
